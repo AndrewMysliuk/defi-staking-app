273c8b3dc30fc831212006ed9d84df68a2bac90f
AndrewMysliuk <andre061120@gmail.com>
2024-06-30 18:54:46 +0300

    feat: basic soliditi contracts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,17 @@
+const Tether = artifacts.require("Tether")
+const RWD = artifacts.require("RWD")
+const DecentralBank = artifacts.require("DecentralBank")
+
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Tether)
+  const tether = await Tether.deployed()
+
+  await deployer.deploy(RWD)
+  const rwd = await RWD.deployed()
+
+  await deployer.deploy(DecentralBank)
+  const decentralBank = DecentralBank.deployed()
+
+  await rwd.transfer(DecentralBank.address, "1000000000000000000000000")
+  await tether.transfer(accounts[1], "1000000000000000000")
+}
